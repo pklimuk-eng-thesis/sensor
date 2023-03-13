eaea6a0e659cf70d0fef3f6df8b3da1f24574b5f
Pavel Klimuk <pavelklimuk@yahoo.com>
2023-03-13 20:11:52 +0100

    Added config reading from .env file

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/pklimuk-eng-thesis/presence-sensor/pkg"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 	r.GET("/isPresenceDetected", pkg.IsPresenceDetected)
 	r.GET("/isSensorEnabled", pkg.IsSensorEnabled)
 	r.POST("/setPresenceDetected", pkg.SetPresenceDetected)
 	r.POST("/setSensorEnabled", pkg.SetSensorEnabled)
-	r.Run("localhost:8080")
+
+	log.Fatal(r.Run(viper.GetString("ADDRESS")))
 }
